8233c4ca69c7e494006e8fc2cae6e2f203c6467a
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-03-22 13:13:06 +0530

    Cli values can be accepted form this program

--- src/client.rs
+++ src/client.rs
@@ -1,5 +1,6 @@
 use hello_world::greeter_client::GreeterClient;
 use hello_world::HelloRequest;
+use std::env;
 
 pub mod hello_world {
     tonic::include_proto!("helloworld");
@@ -8,10 +9,16 @@ pub mod hello_world {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+    let args: Vec<String> = env::args().collect();
+    let name = args.get(1).map(|s| s.as_ref()).unwrap_or("World");
 
     let request = tonic::Request::new(HelloRequest {
-        name: "Jack".into(),
+        name: name.to_string(),
     });
+
+    // let request = tonic::Request::new(HelloRequest {
+    //     name: "Jack".into(),
+    // });
 
     let response = client.say_hello(request).await?;
 
